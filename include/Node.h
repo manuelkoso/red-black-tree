d90f05794fd9d0cde7d6b69925afc7e9ecd9fcb7
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-08-19 15:04:56 +0200

    Moved the constructor definition in the header file

--- include/Node.h
+++ include/Node.h
@@ -1,20 +1,25 @@
 #ifndef RED_BLACK_TREE_NODE_H
 #define RED_BLACK_TREE_NODE_H
 
+#include <iostream>
+
 enum class node_color {red, black};
 
 template<typename T>
 struct Node {
-    Node* parent;
-    Node* right;
-    Node* left;
+    Node<T>* parent;
+    Node<T>* right;
+    Node<T>* left;
     T key;
     node_color color;
 
-    Node(T key);
+    Node(T value): key{value}, color{node_color::red}, parent{nullptr}, right{nullptr}, left{nullptr} {}
 
-    ~Node();
-
+    ~Node() {
+        delete[] parent;
+        delete[] right;
+        delete[] left;
+    }
 };
 
 #endif //RED_BLACK_TREE_NODE_H
--- src/Node.cpp
+++ src/Node.cpp
@@ -1,11 +1,1 @@
 #include "../include/Node.h"
-
-template<typename T>
-Node<T>::Node(T value): key{value}, color{node_color::red}, parent{nullptr}, right{nullptr}, left{nullptr} {}
-
-template<typename T>
-Node<T>::~Node() {
-    delete[] parent;
-    delete[] right;
-    delete[] left;
-}
