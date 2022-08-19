24e38d78244c1a4580dc40e89c437cd4adc0bd65
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-08-19 10:49:41 +0200

    Define the Node struct and enum for colors

--- include/Node.h
+++ include/Node.h
@@ -1,5 +1,15 @@
 #ifndef RED_BLACK_TREE_NODE_H
 #define RED_BLACK_TREE_NODE_H
 
+enum class node_color {red, black};
+
+template<typename T>
+struct Node {
+    Node* parent;
+    Node* right;
+    Node* left;
+    T key;
+    node_color color;
+};
 
 #endif //RED_BLACK_TREE_NODE_H
