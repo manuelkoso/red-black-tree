9b891b0032eea663327a733391b0c4882dd951ad
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-08-19 11:49:18 +0200

    Add root member and decalered private and public methods for inserting nodes into the tree

--- include/RBTree.h
+++ include/RBTree.h
@@ -5,5 +5,20 @@
 #ifndef RED_BLACK_TREE_RBTREE_H
 #define RED_BLACK_TREE_RBTREE_H
 
+#include <functional>
+#include "Node.h"
+
+template<typename T, typename CMP = std::less<T>>
+class RBTree {
+private:
+    Node<T>* root;
+
+    void left_rotate(Node<T>* node);
+    void right_rotate(Node<T>* node);
+    void insert_fixup(Node<T>* node);
+
+public:
+    void insert(const T& value);
+};
 
 #endif //RED_BLACK_TREE_RBTREE_H
