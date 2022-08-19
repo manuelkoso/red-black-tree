ed95ac4a0178ef363ad0607bf8ddae833fb89832
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-08-19 12:19:50 +0200

    Implementation of right_rotate function

--- src/RBTree.cpp
+++ src/RBTree.cpp
@@ -24,3 +24,24 @@ void RBTree<T, CMP>::left_rotate(Node<T>* x) {
     y->left = x;
     x->parent = y;
 }
+
+template<typename T, typename CMP>
+void RBTree<T, CMP>::right_rotate(Node<T>* x) {
+    Node<T>* y = x->left;
+    x->left = y->right;
+    if(y->right != nullptr) {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+
+    if(x->parent == nullptr) {
+        root = y;
+    } else if(x == x->parent->right) {
+        x->parent->right = y;
+    } else {
+        x->parent->right = y;
+    }
+
+    y->right = x;
+    x->parent = y;
+}
