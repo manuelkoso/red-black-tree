ec289fd13d3d7cc80fb7d565604b6c13c5b952c4
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-08-19 15:02:46 +0200

    Added test: Node initialization

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,18 @@
-//
-// Created by koso on 19/08/22.
-//
-
 #define CATCH_CONFIG_MAIN
 #include "../include/catch.hpp"
+#include "../include/Node.h"
+#include "../include/RBTree.h"
+
 
 TEST_CASE("Test") {
     REQUIRE(1 == 1);
 }
 
+TEST_CASE("Node initialization") {
+    Node<int>* node = new Node<int>{5};
+    REQUIRE(node->key == 5);
+    REQUIRE(node->parent == nullptr);
+    REQUIRE(node->right == nullptr);
+    REQUIRE(node->left == nullptr);
+    REQUIRE(node->color == node_color::red);
+}
