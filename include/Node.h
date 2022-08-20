5fed8b4ab413ab80d847575a73ccf7cad106f8af
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-08-20 09:14:15 +0200

    Added custom constructor

--- include/Node.h
+++ include/Node.h
@@ -1,17 +1,23 @@
 #ifndef RED_BLACK_TREE_NODE_H
 #define RED_BLACK_TREE_NODE_H
 
-enum class node_color {red, black};
+enum class node_color {
+    red, black
+};
 
 template<typename T>
 struct Node {
-    Node<T>* parent;
-    Node<T>* right;
-    Node<T>* left;
+    Node<T> *parent;
+    Node<T> *right;
+    Node<T> *left;
     T key;
     node_color color;
 
-    Node(T value): key{value}, color{node_color::red}, parent{nullptr}, right{nullptr}, left{nullptr} {}
+    Node(T value) : key{value}, color{node_color::red}, parent{nullptr}, right{nullptr}, left{nullptr} {}
+
+    Node(Node<T> *p,
+         Node<T> *r, Node<T> *l, T value, node_color color) :
+            parent{p}, right{r}, left{l}, key{value}, color{color}  {}
 
     ~Node() {
         delete[] parent;
