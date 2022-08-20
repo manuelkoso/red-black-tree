edd0a41c4094e7ef16c9f577b80f31e44b344e9a
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-08-20 11:35:04 +0200

    Added test: first insertion

--- test/test.cpp
+++ test/test.cpp
@@ -14,10 +14,16 @@ TEST_CASE("Node initialization") {
     REQUIRE(node->parent == nullptr);
     REQUIRE(node->right == nullptr);
     REQUIRE(node->left == nullptr);
-    REQUIRE(node->color == node_color::red);
+    REQUIRE(node->color == node_color::none);
 }
 
 TEST_CASE("Red black tree initialization") {
     RBTree<int> tree;
     REQUIRE(tree.get_root() == nullptr);
+}
+
+TEST_CASE("First node insertion") {
+    RBTree<int> tree;
+    tree.insert(4);
+    REQUIRE(tree.get_root()->key == 4);
 }
