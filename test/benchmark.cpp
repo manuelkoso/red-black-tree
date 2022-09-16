7fd973147906e117cacf0be2f31017a34308c6b6
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-09-17 00:55:29 +0200

    Added simple benchmark

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -6,30 +6,17 @@
 #include <catch2/catch_all.hpp>
 
 TEST_CASE("Benchmarking") {
-    RBTree<int> my_tree;
+
+    RBTree<int> tree;
     for (auto i = 0; i < 1000000; ++i) {
-        my_tree.insert(i);
+        tree.insert(i);
     }
 
-    std::set<int> set;
-    for (auto i = 0; i < 1000000; ++i) {
-        set.insert(i);
-    }
-
-    BENCHMARK("Find 50000") {
-                                return my_tree.contains(40);
-                            };
-
-    BENCHMARK("Find 100") {
-                              return my_tree.contains(100);
-                          };
-
-    BENCHMARK("Find 50000") {
-                                return set.find(40);
-                            };
-
-    BENCHMARK("Find 100") {
-                              return set.find(100);
-                          };
+    auto value = GENERATE(take(10, random(0, 1000000)));
+    std::string name = "Find node: ";
+    name += std::to_string(value);
+    BENCHMARK(std::move(name)) {
+                                   return tree.contains(value);
+                               };
 
 }
